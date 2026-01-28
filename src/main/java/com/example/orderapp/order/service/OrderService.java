845b1c371c1b49640e6b9ba9a34e7e3668bd0474
jihoon <git40@gmail.com>
2026-01-28 14:50:15 +0900

    Add OrderService

---
+++ src/main/java/com/example/orderapp/order/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.example.orderapp.order.service;
+
+import com.example.orderapp.order.entity.Order;
+import com.example.orderapp.order.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(Long productId, int quantity, String buyerName) {
+        Order order = new Order(productId, quantity, buyerName);
+        return orderRepository.save(order);
+    }
+
+    public List<Order> getOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrder(Long id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("상품을 찾지 못했습니다."));
+    }
+
+}
