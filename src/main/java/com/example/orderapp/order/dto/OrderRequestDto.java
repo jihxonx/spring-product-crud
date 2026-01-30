491e30e3ee7a6793a1bce4087ec6d25ba7d0aca6
jihoon <git40@gmail.com>
2026-01-30 17:02:56 +0900

    refactor: Apply Lombok to OrderRequestDto

--- src/main/java/com/example/orderapp/order/dto/OrderRequestDto.java
+++ src/main/java/com/example/orderapp/order/dto/OrderRequestDto.java
@@ -1,29 +1,16 @@
 package com.example.orderapp.order.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderRequestDto {
     private Long productId;
     private int quantity;
     private String buyerName;
 
-    public OrderRequestDto() {
-
-    }
-
-    public OrderRequestDto(Long productId, int quantity, String buyerName) {
-        this.productId = productId;
-        this.quantity = quantity;
-        this.buyerName = buyerName;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public String getBuyerName() {
-        return buyerName;
-    }
 }
