30faf80f8e044c3aa4c2dee47dc5a9453d6790f8
jihoon <git40@gmail.com>
2026-01-30 16:37:14 +0900

    refactor: Apply Lombok annotations to Order entity
    - Replace manual getters with @Getter
    - Replace protected constructor with @NoArgsConstructor
    - Remove boilerplate code

--- src/main/java/com/example/orderapp/order/entity/Order.java
+++ src/main/java/com/example/orderapp/order/entity/Order.java
@@ -3,9 +3,14 @@ package com.example.orderapp.order.entity;
 
 import com.example.orderapp.product.domain.Product;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "orders")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
@@ -22,29 +27,10 @@ public class Order {
 
     private String buyerName;
 
-    protected Order() {
-
-    }
-
     public Order(Product product, int quantity, String buyerName) {
         this.product = product;
         this.quantity = quantity;
         this.buyerName = buyerName;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public String getBuyerName() {
-        return buyerName;
-    }
 }
