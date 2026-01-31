f52110d2aa6c5bf643caae5b82f9810694c5b589
jihoon <git40@gmail.com>
2026-01-31 23:37:28 +0900

    fate: Create OrderResponseDto for order list response

---
+++ src/main/java/com/example/orderapp/order/dto/OrderResponseDto.java
@@ -0,0 +1,31 @@
+package com.example.orderapp.order.dto;
+
+
+import com.example.orderapp.order.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderResponseDto {
+
+    private Long orderId;
+    private Long productId;
+    private String productName;
+    private int productPrice;
+    private int quantity;
+    private String buyerName;
+
+    public OrderResponseDto(Order order) {
+        this.orderId = order.getId();
+        this.productId = order.getProduct().getId();
+        this.productName = order.getProduct().getName();
+        this.productPrice = order.getProduct().getPrice();
+        this.quantity = order.getQuantity();
+        this.buyerName = order.getBuyerName();
+
+
+    }
+}
