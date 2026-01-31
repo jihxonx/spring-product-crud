bfb04afadb8f3ed3ecce6b3171d4719dbc043077
jihoon <git40@gmail.com>
2026-01-31 23:08:43 +0900

    feat: Add findAllWithProduct method to prevent N+1 problem

--- src/main/java/com/example/orderapp/order/repository/OrderRepository.java
+++ src/main/java/com/example/orderapp/order/repository/OrderRepository.java
@@ -8,6 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    /*'SELECT o FROM Order o' : Order 엔티티를 조회
+    * 'JOIN FETCH o.product' : Order의 product도 함께 조회
+    * JOIN FETCH -> N+1 문제 해결*/
     @Query("SELECT o FROM Order o join FETCH o.product")
     Page<Order> findAllWithProduct(Pageable pageable);
 }
